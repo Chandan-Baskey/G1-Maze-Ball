860aa4932fd2d645eb135964afc558203c2f2689
Underrated Dev <im.chandanbaskey@gmail.com>
2026-06-04 18:13:18 +0530

    Update MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private AudioClip clickSound;
+    private AudioSource audioSource;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,20 +18,37 @@ public class MainMenu : MonoBehaviour
     {
         
     }
+    private void PlayClick()
+    {
+        if (clickSound != null)
+            AudioSource.PlayClipAtPoint(clickSound, Camera.main.transform.position);
+    }
 
     public void StartGame()
     {
-        SceneManager.LoadScene(2);
+        PlayClick();
+        
+        StartCoroutine(LoadWithDelay(2));
+
+        //SceneManager.LoadScene(2);
     }
 
     public void LevelMenu()
     {
-        SceneManager.LoadScene(1);
+        PlayClick();
+        StartCoroutine(LoadWithDelay(1));
+        //SceneManager.LoadScene(1);
     }
 
      public void QuitGame()
     {
+        PlayClick();
         Application.Quit();
         Debug.Log("Quit");
+    }
+    private IEnumerator LoadWithDelay(int sceneIndex)
+    {
+        yield return new WaitForSeconds(0.2f); // wait for click sound
+        SceneManager.LoadScene(sceneIndex);
     }
 }
